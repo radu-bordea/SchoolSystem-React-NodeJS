1ed95081cc79505f3f79e5742b93e364fa452bc2
radu-bordea <77333219+radu-bordea@users.noreply.github.com>
2022-11-10 21:12:55 +0200

    New commit

--- server/index.js
+++ server/index.js
@@ -22,10 +22,41 @@ app.get("/api/teachers", (req, res) => {
      if (err)
        console.error(err);
      else
-        //res.json({"users": ["userOne", "userTwo", "userThree"]})
         res.json({"users": result.rows})
   })
 })
+
+const query1 = `SELECT subject.subjectid, subject.subjectname, student.firstname as stdfirstname,
+student.lastname as stdlastname, teacher.firstname as tchfirstname, teacher.lastname as tchlastname
+FROM student JOIN subject ON student.studentid = subject.studentid
+JOIN teacher ON teacher.teacherid = subject.teacherid
+ORDER BY subject.subjectid;`
+
+
+app.get("/api/participations", (req, res) => {
+  db.query(query1, (err, result) => {
+    if(err)
+      console.error(err)
+    else
+      res.json({"users": result.rows})
+  })
+})
+
+
+const query2 = `SELECT subject.subjectid, subject.subjectname, teacher.firstname as tchfirstname, teacher.lastname as tchlastname
+FROM subject
+JOIN teacher
+on subject.teacherid = teacher.teacherid`
+
+app.get("/api/subjects", (req, res) => {
+  db.query(query2, (err, result) => {
+    if(err)
+      console.error(err)
+    else  
+      res.json({"users": result.rows})
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
